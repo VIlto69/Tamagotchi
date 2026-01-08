524008f43abde3023f4553d2a15c61ec5e3ed256
VIlto69 <agressel@guardiaschool.fr>
2026-01-08 12:32:46 +0100

    Update test_app.py

--- api/test_app.py
+++ api/test_app.py
@@ -1,24 +1,16 @@
 from app import app
 
-
 def test_api_is_alive():
     """Vérifie que la page d'accueil répond bien et que les données sont correctes"""
     with app.test_client() as client:
-        with app.app_context():
-            response = client.get("/")
+        response = client.get("/")
 
-            # Vérifie le code HTTP
-            assert response.status_code == 200
+        assert response.status_code == 200
+        data = response.get_json()
+        assert data is not None
 
-            # Récupère le JSON
-            data = response.get_json()
-            assert data is not None
-
-            # Vérifie les champs
-            assert "name" in data
-            assert "hunger" in data
-            assert "happiness" in data
-
-            # Vérifie les valeurs
-            assert 0 <= data["hunger"] <= 100
-            assert 0 <= data["happiness"] <= 100
+        assert "name" in data
+        assert "hunger" in data
+        assert "happiness" in data
+        assert 0 <= data["hunger"] <= 100
+        assert 0 <= data["happiness"] <= 100
