d7db9d7c0c5ed774f731e5d22e209702b198a51e
squ4le <rlakhssim@guardiaschool.fr>
2026-01-08 12:39:22 +0100

    Fix indentation in test_api_is_alive function

--- api/test_app.py
+++ api/test_app.py
@@ -5,6 +5,5 @@ def test_api_is_alive():
     """Vérifie que la page d'accueil répond bien"""
     with app.test_client() as client:
         response = client.get("/")
-
         # Vérifie le code HTTP
         assert response.status_code == 200
